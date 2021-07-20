06d4e3e1f1ece57568d6527c066b25e477c01436
Connor <connormc757@gmail.com>
2021-07-20 10:01:52 -0400

    added file read / write

--- ising.c
+++ ising.c
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <stdio.h>
 #include <math.h>
+#include "endian.h"
+#include "popcount.h"
 #include "npy_array/npy_array.h"
 
 typedef uint64_t state_t; // several spins in a compressed format for fast batch operations
@@ -126,12 +128,12 @@ double hamiltonian(state_t *lattice, double j)
         //printf("%lx\n", last_carry);
         for (int j = 0; j < SPACE_STATE_COUNT - 1; j++) {
             state_t this_state = lattice[i * SPACE_STATE_COUNT + j];
-            horizontal_energy += __builtin_popcountl(~((last_carry | this_state >> 1) ^ this_state));
+            horizontal_energy += popcount(~((last_carry | this_state >> 1) ^ this_state));
             last_carry = this_state << (SPINS_PER_STATE_T - 1);
         }
         state_t this_state = lattice[(i + 1) * SPACE_STATE_COUNT - 1];
         //printf("this_state: %lx\n", last_carry >> (SPINS_PER_STATE_T - SPACE_REMAINDER) | this_state >> 1);
-        horizontal_energy += __builtin_popcountl(~((last_carry >> (SPINS_PER_STATE_T - SPACE_REMAINDER) | this_state >> 1) ^ this_state) & (state_t)-1 >> (SPINS_PER_STATE_T - SPACE_REMAINDER));
+        horizontal_energy += popcount(~((last_carry >> (SPINS_PER_STATE_T - SPACE_REMAINDER) | this_state >> 1) ^ this_state) & (state_t)-1 >> (SPINS_PER_STATE_T - SPACE_REMAINDER));
     }
     // Now, we need to subtract some constant from it in order to account for the fact that a 0 -> -1 and 1 -> 1
     horizontal_energy = 2 * horizontal_energy - TIME_LEN * SPACE_LEN;
@@ -142,8 +144,8 @@ double hamiltonian(state_t *lattice, double j)
     for (int i = 0; i < TIME_LEN; i++) {
         state_t *current_row = lattice + i * SPACE_STATE_COUNT;
         for (int j = 0; j < SPACE_STATE_COUNT - 1; j++)
-            vertical_energy += __builtin_popcountl(~(current_row[j] ^ above_row[j]));
-        vertical_energy += __builtin_popcountl(~(current_row[SPACE_STATE_COUNT - 1] ^ above_row[SPACE_STATE_COUNT - 1]) & ((state_t)-1 >> (SPINS_PER_STATE_T - SPACE_REMAINDER)));
+            vertical_energy += popcount(~(current_row[j] ^ above_row[j]));
+        vertical_energy += popcount(~(current_row[SPACE_STATE_COUNT - 1] ^ above_row[SPACE_STATE_COUNT - 1]) & ((state_t)-1 >> (SPINS_PER_STATE_T - SPACE_REMAINDER)));
         above_row = current_row;
     }
     // Same deal here
@@ -211,6 +213,128 @@ npy_array_t createNpyDoubleArray1D(size_t count)
     }
     result.shape[0] = count;
     return result;
+}
+
+const uint8_t *file_ver_identifier = "ISI\x01";
+
+int writeHeader(FILE *fp, double j, double beta)
+{
+    if (fwrite(file_ver_identifier, 1, sizeof(file_ver_identifier) - 1, fp) != sizeof(file_ver_identifier) - 1)
+        return -1;
+
+    // make little endian versions of each to write to file
+    // yes, this is ugly, but the alternatives are not as portable
+    uint64_t j_le = htole64(j);
+    if (fwrite(&j_le, sizeof(j_le), 1, fp) != sizeof(j_le))
+        return -1;
+    uint64_t beta_le = htole64(beta);
+    if (fwrite(&beta_le, sizeof(beta_le), 1, fp) != sizeof(beta_le))
+        return -1;
+    uint16_t lattice_time_len = htole16(TIME_LEN);
+    if (fwrite(&lattice_time_len, sizeof(lattice_time_len), 1, fp) != sizeof(lattice_time_len))
+        return -1;
+    uint16_t lattice_space_len = htole16(SPACE_LEN);
+    if (fwrite(&lattice_space_len, sizeof(lattice_space_len), 1, fp) != sizeof(lattice_space_len))
+        return -1;
+    uint16_t state_t_bytes = htole16(sizeof(state_t) * CHAR_BIT / 8);
+    if (fwrite(&state_t_bytes, sizeof(state_t_bytes), 1, fp) != sizeof(state_t_bytes))
+        return -1;
+
+    return 0;
+}
+
+// writes a lattice to the specified FILE *
+// using little-endian byte ordering
+int writeState(FILE *fp, state_t *lattice)
+{
+    char buffer[SPACE_STATE_COUNT * TIME_LEN * sizeof(state_t)];
+    for (size_t i = 0; i < SPACE_STATE_COUNT * TIME_LEN; i++) {
+#if (sizeof(state_t) * CHAR_BIT) == 64
+        buffer[i] = htole64(lattice[i]);
+#elif (sizeof(state_t) * CHAR_BIT) == 32
+        buffer[i] = htole32(lattice[i]);
+#elif (sizeof(state_t) * CHAR_BIT) == 16
+        buffer[i] = htole16(lattice[i]);
+#else
+        buffer[i] = lattice[i];
+#endif
+    }
+
+    if (fwrite(buffer, sizeof(state_t), SPACE_STATE_COUNT * TIME_LEN, fp) != SPACE_STATE_COUNT * TIME_LEN)
+        return -1;
+
+    return 0;
+}
+
+enum {
+    READ_SUCCESS,
+    ERROR_BAD_PREFIX,
+    ERROR_LATTICE_SIZE,
+    ERROR_STATE_SIZE,
+    ERROR_READ,
+};
+
+// read the lattice from the specified FILE pointer
+// assumes all pointers are valid, returns 0 on success
+int readHeader(FILE *fp, double *j, double *beta)
+{
+    uint8_t id_str[sizeof(file_ver_identifier) - 1];
+    if (fread(id_str, 1, sizeof(id_str), fp) != sizeof(id_str))
+        return ERROR_READ;
+
+    if (memcmp(id_str, file_ver_identifier, sizeof(id_str)) != 0)
+        return ERROR_BAD_PREFIX;
+
+    uint64_t j_le;
+    if (fread(&j_le, sizeof(j_le), 1, fp) != sizeof(j_le))
+        return ERROR_READ;
+    *j = (double)le64toh(j_le);
+
+    uint64_t beta_le;
+    if (fread(&beta_le, sizeof(beta_le), 1, fp) != sizeof(beta_le))
+        return ERROR_READ;
+    *beta = (double)le64toh(beta_le);
+
+    uint16_t lattice_time_len;
+    if (fread(&lattice_time_len, sizeof(lattice_time_len), 1, fp) != sizeof(lattice_time_len))
+        return ERROR_READ;
+    if (lattice_time_len != htole16(TIME_LEN))
+        return ERROR_LATTICE_SIZE;
+
+    uint16_t lattice_space_len;
+    if (fread(&lattice_space_len, sizeof(lattice_space_len), 1, fp) != sizeof(lattice_space_len))
+        return ERROR_READ;
+    if (lattice_space_len != htole16(SPACE_LEN))
+        return ERROR_LATTICE_SIZE;
+
+    // could probably adapt for mismatched size at runtime but I don't really feel like it
+    uint16_t state_t_bytes; 
+    if (fread(&state_t_bytes, sizeof(state_t_bytes), 1, fp) != sizeof(state_t_bytes))
+        return ERROR_READ;
+    if (state_t_bytes != htole16(sizeof(state_t) * CHAR_BIT / 8))
+        return ERROR_STATE_SIZE;
+
+    return READ_SUCCESS;
+}
+
+int readLattice(FILE *fp, state_t *lattice)
+{
+    if (fread(lattice, sizeof(state_t), SPACE_STATE_COUNT * TIME_LEN, fp) != SPACE_STATE_COUNT * TIME_LEN)
+        return ERROR_READ;
+    
+    for (int i = 0; i < SPACE_STATE_COUNT * TIME_LEN; i++) {
+#if (sizeof(state_t) * CHAR_BIT) == 64
+        lattice[i] = le64toh(lattice[i]);
+#elif (sizeof(state_t) * CHAR_BIT) == 32
+        lattice[i] = le32toh(lattice[i]);
+#elif (sizeof(state_t) * CHAR_BIT) == 16
+        lattice[i] = le16toh(lattice[i]);
+#else
+        lattice[i] = lattice[i];
+#endif
+    }
+
+    return READ_SUCCESS;
 }
 
 int main(int argc, char **argv)
---
+++ popcount.h
@@ -0,0 +1,36 @@
+int bitcount_lookup[] = {
+    0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+};
+
+int count1sInState(state_t n)
+{
+    int count = 0;
+    for (int i = 0; i < sizeof(state_t); i++)
+        count += bitcount_lookup[((uint8_t *)n)[i]];
+    return count;
+}
+
+#if (defined __GNUC__ && __STDC_VERSION__ >= 201112L)
+#define popcount(x) _Generic((x), \
+        unsigned long: __builtin_popcountl, \
+        unsigned long long: __builtin_popcountll, \
+        unsigned int: __builtin_popcount, \
+        default: count1sInState)(x)
+#else
+#define popcount(x) count1sInState(x)
+#endif
