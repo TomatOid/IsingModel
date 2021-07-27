e4d2a1ebc26f4e0f82643081c1508706497e685d
Connor <connormc757@gmail.com>
2021-07-27 12:05:38 -0400

    added fourier transform correlator

--- correlation.c
+++ correlation.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <complex.h>
+#include <math.h>
 #include "record.h"
 #include "ising.h"
 
@@ -24,6 +26,18 @@ void addToCorrelationArray(state_t *lattice, int *correlation_array)
                 correlation_array[t * SPACE_LEN + x * SPINS_PER_STATE_T + bit] +=
                     2 * (int)(spin_set & 1) - 1;
         }
+    }
+}
+
+// apply fourier transform across the space dimension
+// output should have TIME_LEN complex doubles allocated
+void fourierTransformSpace(state_t *lattice, complex double *output, double p_n)
+{
+    for (int t = 0; t < TIME_LEN; t++) {
+        complex double sum = CMPLX(0, 0);
+        for (int x = 0; x < SPACE_LEN; x++)
+            sum += CMPLX(getSpinAt(lattice, x, t), 0) * cexp(CMPLX(0, p_n * x));
+        output[t] = sum;
     }
 }
 
@@ -53,16 +67,29 @@ int main(int argc, char **argv)
     printf("j: %f, beta: %f\n", j, beta);
 
     int state_counter;
+    complex double correlations[TIME_LEN] = { 0 };
+    int n = 1;
     for (state_counter = 0; readState(data_file, lattice) == READ_SUCCESS; state_counter++) {
-        multiplyLatticeBy(lattice, getSpinAt(lattice, SPACE_LEN / 2, TIME_LEN / 2));
-        addToCorrelationArray(lattice, correlation_array);
+        complex double output[TIME_LEN];
+        fourierTransformSpace(lattice, output, 2 * M_PI * n / SPACE_LEN);
+        for (int i = 0; i < TIME_LEN; i++)
+            correlations[i] += conj(output[0]) * output[i];
     }
+    for (int i = 0; i < TIME_LEN; i++) {
+        printf("%f + %fi, ", creal(correlations[i]) / state_counter, cimag(correlations[i]) / state_counter);
+    }
+    puts("");
+    // int state_counter;
+    // for (state_counter = 0; readState(data_file, lattice) == READ_SUCCESS; state_counter++) {
+    //     multiplyLatticeBy(lattice, getSpinAt(lattice, SPACE_LEN / 2, TIME_LEN / 2));
+    //     addToCorrelationArray(lattice, correlation_array);
+    // }
 
-    npy_array_t output = createNpyDoubleArrayNd(2, TIME_LEN, SPACE_LEN);
+    // npy_array_t output = createNpyDoubleArrayNd(2, TIME_LEN, SPACE_LEN);
 
-    for (int t = 0; t < TIME_LEN; t++)
-        for (int x = 0; x < SPACE_LEN; x++)
-            ((double *)output.data)[t * SPACE_LEN + x] = (double)correlation_array[t * SPACE_LEN + x] / state_counter;
+    // for (int t = 0; t < TIME_LEN; t++)
+    //     for (int x = 0; x < SPACE_LEN; x++)
+    //         ((double *)output.data)[t * SPACE_LEN + x] = (double)correlation_array[t * SPACE_LEN + x] / state_counter;
 
-    npy_array_save(argv[2], &output);
+    // npy_array_save(argv[2], &output);
 }
